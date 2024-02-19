1e8cc5a227136f5e804366e17aab57a33eaafb8a
RealTong <i@wst.sh>
2024-02-19 14:36:53 +0800

    fix(open graph): fix open graph
    fix open graph

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -7,13 +7,13 @@ import siteOgImage from "./og-templates/site";
 const fetchFonts = async () => {
   // Regular Font
   const fontFileRegular = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.regular.ttf"
+    "https://github.com/lxgw/LxgwWenkaiTC/releases/download/v1.320/LXGWWenKaiMonoTC-Regular.ttf"
   );
   const fontRegular: ArrayBuffer = await fontFileRegular.arrayBuffer();
 
   // Bold Font
   const fontFileBold = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.bold.ttf"
+    "https://github.com/lxgw/LxgwWenkaiTC/releases/download/v1.320/LXGWWenKaiTC-Bold.ttf"
   );
   const fontBold: ArrayBuffer = await fontFileBold.arrayBuffer();
 
